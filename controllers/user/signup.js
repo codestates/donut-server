b5be32f3c3c23d06e3e74cf78f55543006daf07b
lwd3737 <lwd3737@naver.com>
2020-09-25 17:01:19 +0900

    jwt

--- controllers/user/signup.js
+++ controllers/user/signup.js
@@ -1,3 +1,28 @@
-module.exports = (req, res) => {
+const { User } = require('../../models');
+ 
+module.exports = async (req, res) => {
 
+    const { email, password, passwordConform, username, address, latlon } = req.body;
+
+    if(password !== passwordConform){
+        return res.status(403).send('Passwords are not identical');
+    }
+
+    [user, created] = await User.findOrCreate({
+        where: {
+            email
+        },
+        defaults: {
+            password,
+            username,
+            address,
+            latlon
+        }
+    });
+
+    if(!created){
+        return res.status(409).send('This email already exists');
+    } 
+
+    return res.status(201).send('Successfully registered');
 };
